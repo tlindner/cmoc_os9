49765f55f017160feaaffec9ef9fc74f94b19478
Boisy Pitre <boisy@mbpro.local>
2018-01-17 06:38:32 -0600

    Converted .as to .c

---
+++ lib/atoi.c
@@ -0,0 +1,51 @@
+asm long
+atol(char *str)
+{
+	asm
+	{
+        pshs  u 
+        ldu   4,s 
+        clra   
+        clrb   
+        pshs  d 
+        pshs  b 
+L000a   ldb   ,u+ 
+        cmpb  #' '          space?
+        beq   L000a 
+        cmpb  #9            tab? 
+        beq   L000a 
+        cmpb  #' '
+        bne   L001c 
+        stb   ,s 
+        bra   L0037 
+L001c   cmpb  #'+'
+        bne   L0039 
+        bra   L0037 
+L0022   ldd   1,s 
+        lslb   
+        rola   
+        lslb   
+        rola   
+        addd  1,s 
+        lslb   
+        rola   
+        pshs  d 
+        ldb   -1,u 
+        clra   
+        subb  #$30 
+        addd  ,s++ 
+        std   1,s 
+L0037   ldb   ,u+ 
+L0039   cmpb  #'0'
+        bcs   L0041 
+        cmpb  #'9'
+        bls   L0022 
+L0041   tst   ,s+ 
+        puls  d 
+        beq   L004b 
+        nega   
+        negb   
+        sbca  #0 
+L004b   puls  u,pc 
+    }
+}
---
+++ lib/atol.c
@@ -0,0 +1,67 @@
+asm long
+atol(char *str)
+{
+	asm
+	{
+_lmul EXTERNAL
+_lneg EXTERNAL
+_flacc EXTERNAL
+_lmove EXTERNAL
+_chcodes EXTERNAL
+        pshs  u 
+        ldu   2+2,s             get pointer to string 
+        clra   
+        clrb   
+        pshs  b 
+        pshs  d 
+        pshs  d 
+charloop
+        ldb   ,u+ 
+        cmpb  #' '              space?
+        beq   charloop 
+        cmpb  #9                tab? 
+        beq   charloop 
+        cmpb  #'-'              negative?
+        bne   L001e 
+        stb   4,s 
+        bra   L0045 
+L001e   cmpb  #'+'
+        bne   L0047 
+        bra   L0045 
+L0024   ldd   2,s 
+        pshs  d 
+        ldd   2,s 
+        pshs  d 
+        leax  >L006c,pcr 
+        lbsr  _lmul 
+        ldb   -1,u 
+        clra   
+        subb  #$30 
+        addd  2,x 
+        std   2,s 
+        ldd   #0 
+        adcb  1,x 
+        adca  ,x 
+        std   ,s 
+L0045   ldb   ,u+ 
+L0047   clra   
+        leax  _chcodes,y 
+        ldb   d,x 
+        andb  #8 
+        bne   L0024 
+        tst   4,s 
+        beq   L005d 
+        leax  ,s 
+        lbsr  _lneg 
+        bra   L005f 
+L005d   leax  ,s 
+L005f   leau  _flacc,y 
+        pshs  u 
+        lbsr  _lmove 
+        leas  5,s 
+        puls  u,pc 
+        
+L006c   fcb $07,$00,$00
+        fcb $07,$00,$0A
+    }
+}
